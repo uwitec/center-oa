82af29d9ad06eccdb7b99eb062c6181195090dcf
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2010-03-08 12:55:55 +0000

    update

--- trunk/sky/src/com/china/center/oa/product/action/ProductAction.java
+++ trunk/sky/src/com/china/center/oa/product/action/ProductAction.java
@@ -12,6 +12,7 @@ import org.apache.struts.actions.DispatchAction;
 
 import com.china.center.common.ConditionParse;
 import com.china.center.oa.product.dao.ProductDAO;
+import com.china.center.oa.product.dao.ProductStatDAO;
 import com.china.center.tools.ActionTools;
 import com.china.center.tools.JSONTools;
 
@@ -23,7 +24,11 @@ public class ProductAction extends DispatchAction
 {
     private ProductDAO productDAO = null;
 
+    private ProductStatDAO productStatDAO = null;
+
     private static String QUERYPRODUCT = "queryProduct";
+
+    private static String QUERYPRODUCTSTAT = "queryProductStat";
 
     /**
      * default constructor
@@ -58,6 +63,32 @@ public class ProductAction extends DispatchAction
     }
 
     /**
+     * queryProductStat
+     * 
+     * @param mapping
+     * @param forms
+     * @param request
+     * @param response
+     * @return
+     * @throws ServletException
+     */
+    public ActionForward queryProductStat(ActionMapping mapping, ActionForm form,
+                                          HttpServletRequest request, HttpServletResponse response)
+        throws ServletException
+    {
+        ConditionParse condtion = new ConditionParse();
+
+        condtion.addWhereStr();
+
+        ActionTools.processJSONQueryCondition(QUERYPRODUCTSTAT, request, condtion);
+
+        String jsonstr = ActionTools.queryVOByJSONAndToString(QUERYPRODUCTSTAT, request, condtion,
+            this.productStatDAO);
+
+        return JSONTools.writeResponse(response, jsonstr);
+    }
+
+    /**
      * @return the productDAO
      */
     public ProductDAO getProductDAO()
@@ -72,5 +103,22 @@ public class ProductAction extends DispatchAction
     public void setProductDAO(ProductDAO productDAO)
     {
         this.productDAO = productDAO;
+    }
+
+    /**
+     * @return the productStatDAO
+     */
+    public ProductStatDAO getProductStatDAO()
+    {
+        return productStatDAO;
+    }
+
+    /**
+     * @param productStatDAO
+     *            the productStatDAO to set
+     */
+    public void setProductStatDAO(ProductStatDAO productStatDAO)
+    {
+        this.productStatDAO = productStatDAO;
     }
 }
