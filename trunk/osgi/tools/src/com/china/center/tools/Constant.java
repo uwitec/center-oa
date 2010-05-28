37c3a4b5532bad1fa0d93386ee19c804d46c607d
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2010-05-28 14:17:02 +0000

    合入bundle

---
+++ trunk/osgi/tools/src/com/china/center/tools/Constant.java
@@ -0,0 +1,25 @@
+/*
+ * File Name: Constant.java
+ * CopyRight: Copyright by www.center.china
+ * Description:
+ * Creater: zhuAchen
+ * CreateTime: 2007-5-15
+ * Grant: open source to everybody
+ */
+package com.china.center.tools;
+
+/**
+ * <描述>
+ * 
+ * @author ZHUZHU
+ * @version 2007-5-15
+ * @see
+ * @since
+ */
+public interface Constant
+{
+    /**
+     * default char set
+     */
+    String DEFAULT_CHAESET = "GBK";
+}
---
+++ trunk/osgi/tools/src/com/china/center/tools/Security.java
@@ -0,0 +1,65 @@
+package com.china.center.tools;
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+/**
+ * 加密类
+ * 
+ * @author public1247
+ * @version
+ * @see Securitys
+ * @since
+ */
+public final class Security
+{
+    /**
+     * Description:  得到一个字符串加密后的结果<br>
+     * [参数列表，说明每个参数用途]
+     *
+     * @param s
+     * @return String
+     */
+    public static final String getSecurity(String s)
+    {
+        final char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f'};
+        try
+        {
+            final byte[] strTemp = s.getBytes();
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            final byte[] md = mdTemp.digest();
+            int j = md.length;
+            char[] str = new char[j << 1];
+            int k = 0;
+            for (int i = 0; i < j; i++ )
+            {
+                byte byte0 = md[i];
+                str[k++ ] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++ ] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            return null;
+        }
+    }
+
+    /**
+	 * Automatically generated method: Security
+	 */
+    private Security() 
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println(getSecurity("123456"));
+    }
+
+}
