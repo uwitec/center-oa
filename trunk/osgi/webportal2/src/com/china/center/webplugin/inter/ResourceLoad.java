2fde3fba21babbb83bca6989336d50ceff51248b
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2010-08-15 14:31:59 +0000

    优化

--- trunk/osgi/webportal2/src/com/china/center/webplugin/inter/ResourceLoad.java
+++ trunk/osgi/webportal2/src/com/china/center/webplugin/inter/ResourceLoad.java
@@ -1,5 +1,6 @@
 package com.china.center.webplugin.inter;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,18 +14,19 @@ import org.osgi.framework.ServiceReference;
 import org.springframework.core.io.Resource;
 import org.springframework.osgi.io.OsgiBundleResourcePatternResolver;
 
+
 /**
  * ResourceLoad
- *  
- * @author  ZHUZHU
- * @version  2009-8-11
- * @see  ResourceLoad
- * @since  1.0
+ * 
+ * @author ZHUZHU
+ * @version 2009-8-11
+ * @see ResourceLoad
+ * @since 1.0
  */
 public class ResourceLoad
 {
     private static final Log LOGGER = LogFactory.getLog(ResourceLoad.class);
-    
+
     /**
      * USER_FLODER
      */
@@ -42,9 +44,9 @@ public class ResourceLoad
             add("com");
         }
     };
-    
-    public static void init(final BundleContext context,
-            final String webPluginName) throws Exception
+
+    public static void init(final BundleContext context, final String webPluginName)
+        throws Exception
     {
         // here must start a new thread to load resource,or applcation may be wait in this function
         new Thread()
@@ -52,24 +54,22 @@ public class ResourceLoad
             public void run()
             {
                 RegisterWebPlugin server = null;
-                
+
                 ServiceReference reference = null;
-                
+
                 if (USER_FLODER.contains(webPluginName.toLowerCase()))
                 {
-                    LOGGER.warn("webPluginName[" + webPluginName + "] in "
-                            + USER_FLODER + ".So stop init.");
-                    
+                    LOGGER.warn("webPluginName[" + webPluginName + "] in " + USER_FLODER + ".So stop init.");
+
                     return;
                 }
-                
-                LOGGER.info("****************IN[" + webPluginName
-                        + "]*****************");
-                
+
+                LOGGER.info("****************IN[" + webPluginName + "]*****************");
+
                 while (server == null)
                 {
                     reference = context.getServiceReference(RegisterWebPlugin.class.getName());
-                    
+
                     if (reference == null)
                     {
                         try
@@ -82,9 +82,9 @@ public class ResourceLoad
                         }
                         continue;
                     }
-                    
-                    server = (RegisterWebPlugin) context.getService(reference);
-                    
+
+                    server = (RegisterWebPlugin)context.getService(reference);
+
                     if (server == null)
                     {
                         try
@@ -97,12 +97,11 @@ public class ResourceLoad
                         }
                     }
                 }
-                
-                LOGGER.info("****************OUT[" + webPluginName
-                        + "]*****************");
-                
+
+                LOGGER.info("****************OUT[" + webPluginName + "]*****************");
+
                 Bundle bundle = context.getBundle();
-                
+
                 try
                 {
                     // first delete resource,then copy resource
@@ -112,50 +111,48 @@ public class ResourceLoad
                 {
                     LOGGER.error(e, e);
                 }
-                
+
                 search(webPluginName, bundle, "osgibundle:webroot/**/*", server);
             }
         }.start();
     }
-    
+
     private static String getPath(String path)
     {
         int index = path.indexOf("webroot/");
-        
+
         if (index == -1)
         {
             return "";
         }
-        
+
         return path.substring(index + "webroot/".length());
     }
-    
+
     private static String getCommonPath(String path)
     {
         return path.replaceAll("\\\\", "/");
     }
-    
-    private static void search(String pnane, Bundle bundle, String pattern,
-            RegisterWebPlugin server)
+
+    private static void search(String pnane, Bundle bundle, String pattern, RegisterWebPlugin server)
     {
-        OsgiBundleResourcePatternResolver patternResolver = new OsgiBundleResourcePatternResolver(
-                bundle);
-        
+        OsgiBundleResourcePatternResolver patternResolver = new OsgiBundleResourcePatternResolver(bundle);
+
         try
         {
             for (Resource resource : patternResolver.getResources(pattern))
             {
                 InputStream in = null;
-                
+
                 try
                 {
                     String path = getCommonPath(getPath(resource.getURI().toString()));
-                    
+
                     if (pnane == null || "".equals(pnane.trim()))
                     {
-                        //防止拷贝受保护的目录
+                        // 防止拷贝受保护的目录
                         boolean protect = false;
-                        
+
                         for (String itemPath : USER_FLODER)
                         {
                             if (path.toLowerCase().startsWith(itemPath + "/"))
@@ -164,20 +161,24 @@ public class ResourceLoad
                                 break;
                             }
                         }
-                        
+
                         if (protect)
                         {
                             continue;
                         }
                     }
-                    
+
+                    // 这里不拷贝.svn和CVS
+                    if (path.indexOf(".svn") != -1 || path.indexOf("CVS") != -1)
+                    {
+                        continue;
+                    }
+
                     if (path != null && resource.exists())
                     {
                         in = resource.getInputStream();
-                        
-                        server.registerWebResource(pnane,
-                                getPath(resource.getURI().toString()),
-                                in);
+
+                        server.registerWebResource(pnane, getPath(resource.getURI().toString()), in);
                     }
                 }
                 catch (Exception e)
@@ -206,51 +207,46 @@ public class ResourceLoad
             return;
         }
     }
-    
-    /** 
+
+    /**
      * detory
-     *
+     * 
      * @param context
      * @param webPluginName
-     * @throws Exception 
+     * @throws Exception
      */
-    public static void destory(final BundleContext context,
-            final String webPluginName) throws Exception
+    public static void destory(final BundleContext context, final String webPluginName)
+        throws Exception
     {
         if (USER_FLODER.contains(webPluginName.toLowerCase()))
         {
-            LOGGER.warn("webPluginName[" + webPluginName + "] in "
-                    + USER_FLODER + ".So stop destory.");
-            
+            LOGGER.warn("webPluginName[" + webPluginName + "] in " + USER_FLODER + ".So stop destory.");
+
             return;
         }
-        
+
         RegisterWebPlugin server = null;
-        
-        LOGGER.info("****************DESTORY-IN[" + webPluginName
-                + "]*****************");
-        
+
+        LOGGER.info("****************DESTORY-IN[" + webPluginName + "]*****************");
+
         ServiceReference reference = context.getServiceReference(RegisterWebPlugin.class.getName());
-        
+
         if (reference == null)
         {
-            LOGGER.info("****************DESTORY-OUT[" + webPluginName
-                    + "]*****************");
+            LOGGER.info("****************DESTORY-OUT[" + webPluginName + "]*****************");
             return;
         }
-        
-        server = (RegisterWebPlugin) context.getService(reference);
-        
+
+        server = (RegisterWebPlugin)context.getService(reference);
+
         if (server == null)
         {
-            LOGGER.info("****************DESTORY-OUT[" + webPluginName
-                    + "]*****************");
+            LOGGER.info("****************DESTORY-OUT[" + webPluginName + "]*****************");
             return;
         }
-        
+
         server.removeWebResource(webPluginName);
-        
-        LOGGER.info("****************DESTORY AND REMOVE-OUT[" + webPluginName
-                + "]*****************");
+
+        LOGGER.info("****************DESTORY AND REMOVE-OUT[" + webPluginName + "]*****************");
     }
 }
