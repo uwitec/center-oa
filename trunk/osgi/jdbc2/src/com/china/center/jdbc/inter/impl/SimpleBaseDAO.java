54368ebdbda8be3804256dbf1d4b6829561d7e61
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2010-11-07 11:16:05 +0000

    优化继承关系

---
+++ trunk/osgi/jdbc2/src/com/china/center/jdbc/inter/impl/SimpleBaseDAO.java
@@ -0,0 +1,25 @@
+/**
+ * File Name: BaseDAO.java<br>
+ * CopyRight: Copyright by www.center.china<br>
+ * Description:<br>
+ * Creater: zhuAchen<br>
+ * CreateTime: 2008-3-10<br>
+ * Grant: open source to everybody
+ */
+package com.china.center.jdbc.inter.impl;
+
+
+import java.io.Serializable;
+
+
+/**
+ * 普通的DAO(独立的DAO版本)
+ * 
+ * @author ZHUZHU
+ * @version 2008-3-10
+ * @see
+ * @since
+ */
+public abstract class SimpleBaseDAO<Bean extends Serializable> extends BaseDAO<Bean, Bean>
+{
+}
