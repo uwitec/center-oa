21dc85385bb9c636f187859adbad5e0b4064d00f
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2011-04-25 15:20:12 +0000

    IN

--- trunk/osgi/webportal2/js/math.js
+++ trunk/osgi/webportal2/js/math.js
@@ -3,47 +3,59 @@ function formatNum(num, length)
 {
 	 var reg = /[0-9]*(.)?[0-9]*$/;
 
-	 if (!reg.test(num))
-	 {
-	 	reg = /[0-9]*.$/;
-	 	if (!reg.test(num))
-	 	{
-	 		return num;
-	 	}
-	 }
+     if (!reg.test(num))
+     {
+        reg = /[0-9]*.$/;
+        if (!reg.test(num))
+        {
+            return num;
+        }
+     }
 
-	 num += '';
-	 
-	 if (num.indexOf('.') == -1)
-	 {
-	 	return num + '.' + getLength0(length);
-	 }
+     num += '';
+     
+     if (num.indexOf('.') == -1)
+     {
+        return num + '.' + getLength0(length);
+     }
 
-	 var hou = num.substring(num.indexOf('.') + 1);
+     var hou = num.substring(num.indexOf('.') + 1);
 
-	 if (hou.length <= length)
-	 {
-	 	return num + getLength0(length - hou.length);
-	 }
+     if (hou.length <= length)
+     {
+        return num + getLength0(length - hou.length);
+     }
 
-	 //超过 指定的四舍五入
-	 var ins = parseFloat(num.substring(0, num.indexOf('.') + 1) + hou.substring(0, length));
+     //超过 指定的四舍五入
+     var ins = parseFloat(num.substring(0, num.indexOf('.') + 1) + hou.substring(0, length));
 
-	 var last = parseInt(hou.charAt(length));
-	 var add;
+     var last = parseInt(hou.charAt(length));
+     var add;
 
-	 if (last >= 5)
-	 {
-	 	add = '0.' + getLength0(length - 1) + '1';
-	 }
-	 else
-	 {
-	 	add = '0.' + getLength0(length);
-	 }
+     if (last >= 5)
+     {
+        add = '0.' + getLength0(length - 1) + '1';
+     }
+     else
+     {
+        add = '0.' + getLength0(length);
+     }
 
-	 var result =  ins + parseFloat(add);
+     var result =  ins + parseFloat(add);
 
-	 return result + '';
+     var sresult = result + '';
+     
+     if (sresult.indexOf('.') == -1)
+     {
+        return sresult;
+     }
+     
+     if (sresult.indexOf('.') != -1)
+     {
+         sresult = sresult + '00';           
+     }
+     
+     return sresult.substring(0, sresult.indexOf('.') + 3);
 }
 
 function getLength0(length)
