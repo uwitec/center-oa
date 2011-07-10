844ecd8e01f2652d38287a0c4254829d3fc25d37
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2011-07-10 13:59:11 +0000

    IN

---
+++ trunk/osgi/oa_tcp_inner/src/com/china/center/oa/tcp/dao/impl/TravelApplyDAOImpl.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: TravelApplyDAOImpl.java<br>
+ * CopyRight: Copyright by www.center.china<br>
+ * Description:<br>
+ * CREATER: ZHUACHEN<br>
+ * CreateTime: 2011-7-10<br>
+ * Grant: open source to everybody
+ */
+package com.china.center.oa.tcp.dao.impl;
+
+
+import com.china.center.jdbc.inter.impl.BaseDAO;
+import com.china.center.oa.tcp.bean.TravelApplyBean;
+import com.china.center.oa.tcp.dao.TravelApplyDAO;
+import com.china.center.oa.tcp.vo.TravelApplyVO;
+
+
+/**
+ * TravelApplyDAOImpl
+ * 
+ * @author ZHUZHU
+ * @version 2011-7-10
+ * @see TravelApplyDAOImpl
+ * @since 3.0
+ */
+public class TravelApplyDAOImpl extends BaseDAO<TravelApplyBean, TravelApplyVO> implements TravelApplyDAO
+{
+}
