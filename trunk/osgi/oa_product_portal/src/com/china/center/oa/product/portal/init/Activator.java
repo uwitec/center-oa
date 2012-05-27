6959e2d1f3e64961c7f86ea9444b2f867b8e9144
zhu.000@163.com <zhu.000@163.com@9ae9f79a-9542-11dd-90d1-a37c03efd28a>
2012-05-27 02:25:55 +0000

    IN

--- trunk/osgi/oa_product_portal/src/com/china/center/oa/product/portal/init/Activator.java
+++ trunk/osgi/oa_product_portal/src/com/china/center/oa/product/portal/init/Activator.java
@@ -37,7 +37,7 @@ public class Activator implements BundleActivator
         // 加载JSP资源到WEBAPP下面
         ResourceLoad.init(context, "");
 
-        FilterLoad.loadIgnoreFilterMatch(filterMatchList);
+        FilterLoad.putIgnoreFilterMatch(filterMatchList);
 
     }
 
@@ -50,7 +50,7 @@ public class Activator implements BundleActivator
             ResourceLoad.destory(context, each);
         }
 
-        FilterLoad.loadIgnoreFilterMatch(filterMatchList);
+        FilterLoad.removeIgnoreFilterMatch(filterMatchList);
     }
 
 }
